897e85b7a9fecb7a4d493295ba4efe344b44199f
dantegarciadev <zickdante2015@gmail.com>
2023-06-10 15:25:34 -0300

    actualizar archivos

--- funciones.py
+++ funciones.py
@@ -3,6 +3,9 @@ from tkinter import filedialog
 import tkinter as tk
 import customtkinter as ctk
 from customtkinter import filedialog
+from datetime import datetime
+
+
 
 
 def seleccionar_archivo():
@@ -11,3 +14,15 @@ def seleccionar_archivo():
     file_path = filedialog.askopenfile()
     return file_path
 
+#FUNCION TRANSFORMAR FECHAS
+
+
+def transformar_fecha(fecha):
+    patrones = ["%d-%m-%Y", "%d%m%Y", "%Y-%m-%d", "%d-%B-%Y","%d/%m/%Y %H:%M:%S" ]
+    for patron in patrones:
+        try:
+            fecha_transformada = datetime.strptime(fecha, patron).strftime("%d/%m/%Y")
+            return fecha_transformada
+        except ValueError:
+            pass
+    return None
