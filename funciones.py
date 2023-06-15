14f880dc43c777d3b71382eb9283175d894ed68e
dantegarciadev <zickdante2015@gmail.com>
2023-06-14 22:18:38 -0300

    corregida funcion para normalizar fechas

--- funciones.py
+++ funciones.py
@@ -4,6 +4,7 @@ import tkinter as tk
 import customtkinter as ctk
 from customtkinter import filedialog
 from datetime import datetime
+import pandas as pd
 
 
 
@@ -17,12 +18,19 @@ def seleccionar_archivo():
 #FUNCION TRANSFORMAR FECHAS
 
 
-def transformar_fecha(fecha):
-    patrones = ["%d-%m-%Y", "%d%m%Y", "%Y-%m-%d", "%d-%B-%Y","%d/%m/%Y %H:%M:%S" ]
-    for patron in patrones:
-        try:
-            fecha_transformada = datetime.strptime(fecha, patron).strftime("%d/%m/%Y")
-            return fecha_transformada
-        except ValueError:
-            pass
-    return None
+#def transformar_fecha(fecha):
+ #   patrones = ["%d-%m-%Y", "%d%m%Y", "%Y-%m-%d", "%d-%B-%Y","%d/%m/%Y %H:%M:%S" ]
+  #  for patron in patrones:
+   #     try:
+    #        fecha_transformada = datetime.strptime(fecha, patron).strftime("%d/%m/%Y")
+     #       return fecha_transformada
+      #  except ValueError:
+       #     pass
+  #  return None
+  
+def transformar_fecha(df, columna):
+    df[columna]=pd.to_datetime(df[columna], dayfirst=False, errors='coerce')
+    df[columna] = df[columna].fillna('01/01/1900')
+    df[columna] = df[columna].dt.strftime("%d/%m/%Y")
+    return df
+    
