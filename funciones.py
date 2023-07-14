52e7f18782fbe2d44e23fd2c8e827b345bf0a935
dantegarciadev <zickdante2015@gmail.com>
2023-07-13 22:23:14 -0300

    padron tis

--- funciones.py
+++ funciones.py
@@ -19,19 +19,19 @@ def seleccionar_archivo():
 #FUNCION TRANSFORMAR FECHAS
 
 
-#def transformar_fecha2(fecha):
- #   patrones = ["%d-%m-%Y", "%d%m%Y", "%Y-%m-%d", "%d-%B-%Y","%d/%m/%Y %H:%M:%S" ]
-  #  for patron in patrones:
-   #     try:
-    #        fecha_transformada = datetime.strptime(fecha, patron).strftime("%d/%m/%Y")
-     #       return fecha_transformada
-      #  except ValueError:
-       #     pass
-  #  return None
+def transformar_fecha2(fecha):
+    patrones = ["%d-%m-%Y", "%d%m%Y", "%Y-%m-%d", "%d-%B-%Y","%d/%m/%Y %H:%M:%S","%Y-%m-%d %H:%M:%S" ]
+    for patron in patrones:
+        try:
+            fecha_transformada = datetime.strptime(fecha, patron).strftime("%d-%m-%Y")
+            return fecha_transformada
+        except ValueError:
+            pass
+    return None
   
-def transformar_fecha(df, columna):
-    df[columna]=pd.to_datetime(df[columna], dayfirst=True, errors='coerce')
-    df[columna] = df[columna].fillna('01/01/1900')
-    df[columna] = df[columna].dt.strftime("%d/%m/%Y")
-    return df
+#def transformar_fecha(df, columna):
+ #   df[columna]=pd.to_datetime(df[columna], dayfirst=True, errors='coerce')
+  #  df[columna] = df[columna].fillna('01-01-1900')
+   # df[columna] = df[columna].dt.strftime("%d-%m-%Y")
+    #return df
     
